6aef78061c145a83020677952c01e1f7b15de51d
C1330638 Renan Matias Mendes <c1330638@interno.bb.com.br>
2025-05-21 21:39:00 -0300

    configurando paginas

---
+++ pages/quem-somos/index.tsx
@@ -0,0 +1,14 @@
+import styles from './styles.module.css'
+
+export default function QuemSomos(){
+    return (
+        < >
+            <h1>Quem Somos</h1>
+            <p></p>
+        
+        
+        
+        
+        </>
+    );
+}
